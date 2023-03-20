bd13f93d3bfcc86a42b258c1781588fed9793e11
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-03-20 18:08:23 +0900

    init: 초기 세팅

---
+++ src/main/java/project/volunteer/global/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package project.volunteer.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .build();
+    }
+}
---
+++ src/test/java/project/volunteer/VolunteerApplicationTests.java
@@ -0,0 +1,13 @@
+package project.volunteer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
