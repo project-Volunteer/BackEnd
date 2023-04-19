c24d3034cc562578a1e4edd86602ffaaeef1b37c
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-04-20 04:31:04 +0900

    fix: null list의 stream 오류 해결

--- src/main/java/project/volunteer/domain/recruitment/dao/queryDto/dto/RecruitmentCond.java
+++ src/main/java/project/volunteer/domain/recruitment/dao/queryDto/dto/RecruitmentCond.java
@@ -3,18 +3,23 @@ package project.volunteer.domain.recruitment.dao.queryDto.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.util.StringUtils;
 import project.volunteer.domain.recruitment.domain.VolunteerType;
 import project.volunteer.domain.recruitment.domain.VolunteeringCategory;
 import project.volunteer.domain.recruitment.domain.VolunteeringType;
 import project.volunteer.global.util.LegacyCodeEnumValueConverterUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Slf4j
 public class RecruitmentCond {
 
     private List<VolunteeringCategory> category;
@@ -26,9 +31,11 @@ public class RecruitmentCond {
 
     public RecruitmentCond(List<String> category, String sido, String sigungu, String volunteeringType, String volunteerType, Boolean isIssued) {
 
-        this.category = category.stream()
-                .map(c -> LegacyCodeEnumValueConverterUtils.ofLegacyCode(VolunteeringCategory.class, c))
-                .collect(Collectors.toList());
+        this.category = Optional.ofNullable(category)
+                .orElseGet(Collections::emptyList)
+                        .stream()
+                                .map(c -> LegacyCodeEnumValueConverterUtils.ofLegacyCode(VolunteeringCategory.class, c))
+                                        .collect(Collectors.toList());
         this.sido = sido;
         this.sigungu = sigungu;
         this.volunteeringType = (StringUtils.hasText(volunteeringType))?(VolunteeringType.of(volunteeringType)):null;
