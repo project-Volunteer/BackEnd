f4803e1c8b76c0375616cf56ed8b42f1f1707d0f
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-05-03 08:33:05 +0900

    feat: unckecked exceptions 관리를 위한 baseException 구현

---
+++ src/main/java/project/volunteer/global/error/exception/BaseException.java
@@ -0,0 +1,28 @@
+package project.volunteer.global.error.exception;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseException extends RuntimeException{
+
+    //예외 코드
+    protected ErrorCode errorCode;
+
+    //예외 message 만들때 사용할 매개변수를 담은 배열
+    protected Object[] args;
+
+    //추가 상세 메시지
+    protected String details;
+
+    public BaseException(ErrorCode errorCode){
+        this.errorCode = errorCode;
+    }
+    public BaseException(ErrorCode errorCode, String details){
+        this.errorCode = errorCode;
+        this.details = details;
+    }
+
+}
---
+++ src/main/java/project/volunteer/global/error/exception/ErrorCode.java
@@ -0,0 +1,28 @@
+package project.volunteer.global.error.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    //모집글 관련
+    NOT_EXIST_RECRUITMENT(HttpStatus.BAD_REQUEST, "recruitment.not_exist"),
+
+    //사용자 관련
+    NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "user.not_exist"),
+    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "user.unauthorized"),
+
+    //파일 관련
+    INVALID_FILE_FORMAT(HttpStatus.BAD_REQUEST, "file.format.invalid"),
+    NOT_FOUND_FILE_FOLDER(HttpStatus.INTERNAL_SERVER_ERROR, "file.folder.not_found"),
+    S3_UPLOAD_IO_EX(HttpStatus.INTERNAL_SERVER_ERROR, "file.upload.io_ex")
+
+    ;
+
+    private final HttpStatus httpStatus; //상태 코드
+    private final String propertiesCode; //메시지 코드
+
+}
