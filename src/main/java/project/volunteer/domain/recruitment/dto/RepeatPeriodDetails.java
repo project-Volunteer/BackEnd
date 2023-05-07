83376390a33a52b2156349cdd41791d90b1543c1
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-05-08 03:36:39 +0900

    hotfix: 주기가 매주일 경우 발생하는 Null 버그 수정

--- src/main/java/project/volunteer/domain/recruitment/dto/RepeatPeriodDetails.java
+++ src/main/java/project/volunteer/domain/recruitment/dto/RepeatPeriodDetails.java
@@ -4,12 +4,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import project.volunteer.domain.repeatPeriod.domain.Day;
-import project.volunteer.domain.repeatPeriod.domain.Period;
-import project.volunteer.domain.repeatPeriod.domain.Week;
-
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -21,12 +16,10 @@ public class RepeatPeriodDetails {
     private String week;
     private List<String> days;
 
-    public RepeatPeriodDetails(Period period, Week week, List<Day> days){
-        this.period = period.getViewName();
-        this.week = week.getViewName();
-        this.days = days.stream()
-                .map(r -> new String(r.getViewName()))
-                .collect(Collectors.toList());
+    public RepeatPeriodDetails(String period, String week, List<String> days){
+        this.period = period;
+        this.week = week;
+        this.days = days;
     }
 
 }
