f77e4d9ea205ab7ce0b0b864c517863c0c508722
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-05-12 02:07:16 +0900

    feat: Error code 및 message 추가

--- src/main/java/project/volunteer/global/error/exception/ErrorCode.java
+++ src/main/java/project/volunteer/global/error/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode {
 
     //모집글 관련
     NOT_EXIST_RECRUITMENT(HttpStatus.BAD_REQUEST, "notExist.recruitment"),
+    FORBIDDEN_RECRUITMENT(HttpStatus.FORBIDDEN, "forbidden.recruitment"),
 
     //사용자 관련
     NOT_EXIST_USER(HttpStatus.BAD_REQUEST, "notExist.user"),
@@ -25,8 +26,11 @@ public enum ErrorCode {
     INVALID_FILE_FORMAT(HttpStatus.BAD_REQUEST, "invalid.file.format"),
     NOT_FOUND_FILE_FOLDER(HttpStatus.INTERNAL_SERVER_ERROR, "notFound.file.folder"),
     NOT_FOUND_FILE_FOLDER_OfCode(HttpStatus.INTERNAL_SERVER_ERROR, "notFound.file.folder"),
-    S3_UPLOAD_IO_EX(HttpStatus.INTERNAL_SERVER_ERROR, "io.file.upload")
+    S3_UPLOAD_IO_EX(HttpStatus.INTERNAL_SERVER_ERROR, "io.file.upload"),
 
+    //모집글 팀원 관련
+    DUPLICATE_PARTICIPATION(HttpStatus.BAD_REQUEST, "duplicate.participation"),
+    INVALID_STATE(HttpStatus.BAD_REQUEST, "invalid.state"),
     ;
 
     private final HttpStatus httpStatus; //상태 코드
