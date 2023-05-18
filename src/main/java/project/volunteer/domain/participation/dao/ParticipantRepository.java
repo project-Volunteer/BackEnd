cc5ba9e38297c41b72c1560504ff77fb59455deb
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-05-19 01:12:13 +0900

    refactor: 참여자 리스트 조회 쿼리 성능 최적화

--- src/main/java/project/volunteer/domain/participation/dao/ParticipantRepository.java
+++ src/main/java/project/volunteer/domain/participation/dao/ParticipantRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import project.volunteer.domain.participation.dao.dto.ParticipantStateDetails;
 import project.volunteer.domain.participation.domain.Participant;
 import project.volunteer.global.common.component.State;
 
@@ -15,6 +16,19 @@ public interface ParticipantRepository extends JpaRepository<Participant, Long> 
     //참여자 매핑 정보, 참여자 정보 쿼리 한번에 가져오기(left join)
     @EntityGraph(attributePaths = {"participant"})
     List<Participant> findEGParticipantByRecruitment_RecruitmentNoAndStateIn(Long recruitmentNo, List<State> states);
+
+    @Query("select new project.volunteer.domain.participation.dao.dto.ParticipantStateDetails" +
+            "(p.state, u.userNo, u.nickName, coalesce(s.imagePath, u.picture)) " +
+            "from Participant p " +
+            "join p.participant as u " +
+            "left join Image i " +
+            "on u.userNo = i.no " +
+            "and i.realWorkCode=project.volunteer.domain.image.domain.RealWorkCode.USER " +
+            "and i.isDeleted=project.volunteer.global.common.component.IsDeleted.N " +
+            "left join i.storage as s " +
+            "where p.recruitment.recruitmentNo=:no " +
+            "and p.state in :states ")
+    List<ParticipantStateDetails> findParticipantsByOptimization(@Param("no") Long recruitmentNo, @Param("states") List<State> states);
 
     Optional<Participant> findByRecruitment_RecruitmentNoAndParticipant_UserNo(Long recruitmentNo, Long userId);
 
