046055635da4cf89b1e73844f0ecbcfb0743a42c
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-06-10 16:45:44 +0900

    feat: 추가 권한 검증 인터셉터 기능 구현

---
+++ src/main/java/project/volunteer/global/Interceptor/OrganizationAuth.java
@@ -0,0 +1,18 @@
+package project.volunteer.global.Interceptor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface OrganizationAuth {
+
+    Auth auth();
+
+    enum Auth{
+        ORGANIZATION_ADMIN, //봉사 모집글 방장
+        ORGANIZATION_TEAM //봉사 모집글 팀원
+    }
+}
