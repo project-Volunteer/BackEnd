679ec2b80509b065f3e1654d527a7cb0884bcbb1
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-06-30 16:55:01 +0900

    refactor: cron 시간 수정

--- src/main/java/project/volunteer/global/common/scheduler/Scheduler.java
+++ src/main/java/project/volunteer/global/common/scheduler/Scheduler.java
@@ -13,7 +13,7 @@ public class Scheduler {
 
     private final ScheduleService scheduleService;
 
-    @Scheduled(cron = "0 6 17 * * * ", zone = "Asia/Seoul") //매 00시 01분 마다 실행
+    @Scheduled(cron = "0 1 0 * * * ", zone = "Asia/Seoul") //매 00시 01분 마다 실행
     public void scheduleCompletionValidation(){
 
         log.info("마감 일정 참가자 상태 자동 업데이트 스케줄러 시작");
