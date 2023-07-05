c5fb786cf32b2d3b78ffe55e021df8595bcea65b
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-07-05 17:46:52 +0900

    test: 봉사 모집글 공지사항 읽음,해제 Service 통합 테스트 코드 추가

--- src/test/java/project/volunteer/domain/notice/application/NoticeDtoServiceImplTest.java
+++ src/test/java/project/volunteer/domain/notice/application/NoticeDtoServiceImplTest.java
@@ -1,11 +1,14 @@
 package project.volunteer.domain.notice.application;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import project.volunteer.domain.confirmation.dao.ConfirmationRepository;
+import project.volunteer.domain.confirmation.domain.Confirmation;
 import project.volunteer.domain.notice.application.dto.NoticeDetails;
 import project.volunteer.domain.notice.dao.NoticeRepository;
 import project.volunteer.domain.notice.domain.Notice;
@@ -18,10 +21,7 @@ import project.volunteer.domain.user.dao.UserRepository;
 import project.volunteer.domain.user.domain.Gender;
 import project.volunteer.domain.user.domain.Role;
 import project.volunteer.domain.user.domain.User;
-import project.volunteer.global.common.component.Address;
-import project.volunteer.global.common.component.Coordinate;
-import project.volunteer.global.common.component.HourFormat;
-import project.volunteer.global.common.component.Timetable;
+import project.volunteer.global.common.component.*;
 
 import javax.persistence.EntityManager;
 import java.time.LocalDate;
@@ -29,6 +29,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SpringBootTest
 class NoticeDtoServiceImplTest {
@@ -37,6 +38,7 @@ class NoticeDtoServiceImplTest {
     @Autowired UserRepository userRepository;
     @Autowired RecruitmentRepository recruitmentRepository;
     @Autowired NoticeRepository noticeRepository;
+    @Autowired ConfirmationRepository confirmationRepository;
     @Autowired NoticeDtoService noticeDtoService;
 
     User writer;
@@ -97,6 +99,60 @@ class NoticeDtoServiceImplTest {
         assertThat(noticeDto.getCheckCnt()).isEqualTo(0);
     }
 
+    @Test
+    @DisplayName("읽음 확인한 공지사항 상세 조회에 성공하다.")
+    @Transactional
+    public void findNoticeDetails_read(){
+        //given
+        final String content = "notice";
+        User loginUser = 사용자_추가("wow");
+        Notice notice = 공지사항_등록(content, saveRecruitment);
+        읽음_등록(RealWorkCode.NOTICE, notice.getNoticeNo(), loginUser);
+        notice.increaseCheckNum();
+        clear();
+
+        //when
+        NoticeDetails noticeDto = noticeDtoService.findNoticeDto(saveRecruitment.getRecruitmentNo(), notice.getNoticeNo(), loginUser.getUserNo());
+
+        //then
+        assertThat(noticeDto.getIsChecked()).isTrue();
+        assertThat(noticeDto.getCheckCnt()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("3개 중 2개를 읽음 확인한 공지사항 리스트 조회에 성공하다.")
+    @Transactional
+    public void findNoticeDetails_readCancel(){
+        //given
+        User loginUser = 사용자_추가("wow");
+
+        Notice notice1 = 공지사항_등록("notice1", saveRecruitment);
+        읽음_등록(RealWorkCode.NOTICE, notice1.getNoticeNo(), loginUser);
+        notice1.increaseCheckNum();
+
+        Notice notice2 = 공지사항_등록("notice2", saveRecruitment);
+        읽음_등록(RealWorkCode.NOTICE, notice2.getNoticeNo(), loginUser);
+        notice2.increaseCheckNum();
+
+        Notice notice3 = 공지사항_등록("notice3", saveRecruitment);
+        clear();
+
+        //when
+        List<NoticeDetails> findDtos = noticeDtoService.findNoticeDtos(saveRecruitment.getRecruitmentNo(), loginUser.getUserNo());
+
+        //then
+        assertAll(
+                () -> assertThat(findDtos.size()).isEqualTo(3),
+                () -> assertThat(findDtos.get(0).getCheckCnt()).isEqualTo(1),
+                () -> assertThat(findDtos.get(0).getIsChecked()).isTrue(),
+                () -> assertThat(findDtos.get(1).getCheckCnt()).isEqualTo(1),
+                () -> assertThat(findDtos.get(1).getIsChecked()).isTrue(),
+                () -> assertThat(findDtos.get(2).getCheckCnt()).isEqualTo(0),
+                () -> assertThat(findDtos.get(2).getIsChecked()).isFalse()
+        );
+    }
+
+
     private void clear() {
         em.flush();
         em.clear();
@@ -110,5 +166,10 @@ class NoticeDtoServiceImplTest {
         Notice createNotice = Notice.createNotice(content);
         createNotice.setRecruitment(recruitment);
         return noticeRepository.save(createNotice);
+    }
+    private Confirmation 읽음_등록(RealWorkCode code, Long no, User user){
+        Confirmation createConfirmation = Confirmation.createConfirmation(code, no);
+        createConfirmation.setUser(user);
+        return confirmationRepository.save(createConfirmation);
     }
 }
