335350de4dc6f4d4579fcd24e5516e10e69e0c38
BonSik-Koo <rnqhstlr2297@gmail.com>
2023-08-05 00:55:51 +0900

    feat: Enum 클래스 Rest docs 문서화 추가

--- src/test/java/project/volunteer/restdocs/document/CommonDocControllerTest.java
+++ src/test/java/project/volunteer/restdocs/document/CommonDocControllerTest.java
@@ -1,6 +1,5 @@
 package project.volunteer.restdocs.document;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
@@ -9,6 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.payload.PayloadSubsectionExtractor;
 import org.springframework.test.web.servlet.MockMvc;
+import project.volunteer.domain.image.domain.ImageType;
+import project.volunteer.domain.recruitment.domain.VolunteerType;
+import project.volunteer.domain.recruitment.domain.VolunteeringCategory;
+import project.volunteer.domain.recruitment.domain.VolunteeringType;
+import project.volunteer.domain.repeatPeriod.domain.Day;
+import project.volunteer.domain.repeatPeriod.domain.Period;
+import project.volunteer.domain.repeatPeriod.domain.Week;
 import project.volunteer.global.common.component.HourFormat;
 import project.volunteer.global.common.converter.CodeCommonType;
 import project.volunteer.global.common.dto.StateResponse;
@@ -40,6 +46,34 @@ public class CommonDocControllerTest {
                         customResponseFields("custom-response", beneathPath("data.clientState").withSubsectionId("clientState"),
                                 attributes(key("title").value("클라이언트 신청 상태")),
                                 enumConvertFieldDescriptor(StateResponse.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.volunteeringCategory").withSubsectionId("volunteeringCategory"),
+                                attributes(key("title").value("봉사 유형 카테고리")),
+                                enumConvertFieldDescriptor(VolunteeringCategory.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.volunteerType").withSubsectionId("volunteerType"),
+                                attributes(key("title").value("봉사자 유형")),
+                                enumConvertFieldDescriptor(VolunteerType.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.volunteeringType").withSubsectionId("volunteeringType"),
+                                attributes(key("title").value("봉사 타입")),
+                                enumConvertFieldDescriptor(VolunteeringType.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.period").withSubsectionId("period"),
+                                attributes(key("title").value("반복 주기")),
+                                enumConvertFieldDescriptor(Period.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.week").withSubsectionId("week"),
+                                attributes(key("title").value("반복 주")),
+                                enumConvertFieldDescriptor(Week.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.day").withSubsectionId("day"),
+                                attributes(key("title").value("반복 요일")),
+                                enumConvertFieldDescriptor(Day.values())
+                        ),
+                        customResponseFields("custom-response", beneathPath("data.imageType").withSubsectionId("imageType"),
+                                attributes(key("title").value("이미지 타입")),
+                                enumConvertFieldDescriptor(ImageType.values())
                         )
                 ));
     }
--- src/test/java/project/volunteer/restdocs/document/dto/EnumDocs.java
+++ src/test/java/project/volunteer/restdocs/document/dto/EnumDocs.java
@@ -11,4 +11,11 @@ public class EnumDocs {
     // 문서화하고 싶은 모든 enum 값을 명시
     Map<String,String> hourFormat;
     Map<String,String> clientState;
+    Map<String,String> volunteeringCategory;
+    Map<String,String> volunteerType;
+    Map<String,String> volunteeringType;
+    Map<String,String> period;
+    Map<String,String> week;
+    Map<String,String> day;
+    Map<String,String> imageType;
 }
