5c23e451d84121316852560d93dd6e2aa760aee2
BonSik <rnqhstlr2297@gmail.com>
2024-02-14 01:24:51 +0900

    test: 봉사 모집글 상세 조회 API 인수 테스트 추가

--- src/test/java/project/volunteer/acceptance/RecruitmentAcceptanceTest.java
+++ src/test/java/project/volunteer/acceptance/RecruitmentAcceptanceTest.java
@@ -1,14 +1,23 @@
 package project.volunteer.acceptance;
 
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.BDDMockito.given;
 import static project.volunteer.acceptance.AcceptanceFixtures.봉사_게시물_등록;
+import static project.volunteer.acceptance.AcceptanceFixtures.봉사_게시물_팀원_가입_승인;
+import static project.volunteer.acceptance.AcceptanceFixtures.봉사_게시물_팀원_가입_요청;
 
 import java.io.File;
+import java.time.Instant;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import project.volunteer.domain.participation.api.dto.request.ParticipantAddParam;
+import project.volunteer.domain.recruitment.application.dto.query.detail.RecruitmentDetailSearchResult;
 import project.volunteer.domain.recruitment.domain.VolunteerType;
 import project.volunteer.domain.recruitment.domain.VolunteeringCategory;
 import project.volunteer.domain.recruitment.domain.VolunteeringType;
@@ -92,6 +101,46 @@ public class RecruitmentAcceptanceTest extends AcceptanceTest {
                 .then().log().all()
                 .statusCode(HttpStatus.FORBIDDEN.value())
                 .extract();
+    }
+
+    @DisplayName("특정 봉사 모집글 정보를 상세 조회한다.")
+    @Test
+    void findRecruitment() {
+        given(clock.instant()).willReturn(Instant.parse("2024-02-02T10:00:00Z"));
+
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 100, VolunteeringType.REG, "01-01-2024", "02-10-2024", HourFormat.AM, "10:00",
+                10,
+                Period.WEEK, Week.NONE, List.of(Day.MON, Day.FRI), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        Long participantNo1 = 봉사_게시물_팀원_가입_요청(soeunToken, recruitmentNo);
+        Long participantNo2 = 봉사_게시물_팀원_가입_요청(changHoeunToken, recruitmentNo);
+
+        봉사_게시물_팀원_가입_승인(bonsikToken, recruitmentNo, new ParticipantAddParam(List.of(participantNo1)));
+
+        RecruitmentDetailSearchResult response = given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .header(AUTHORIZATION_HEADER, soeunToken)
+                .when().get("/recruitment/{no}", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(RecruitmentDetailSearchResult.class);
+        assertAll(
+                () -> assertThat(response.getNo()).isEqualTo(recruitmentNo),
+                () -> assertThat(response.getRepeatPeriod().getPeriod()).isEqualTo(Period.WEEK.getId()),
+                () -> assertThat(response.getRepeatPeriod().getWeek()).isEqualTo(Week.NONE.getId()),
+                () -> assertThat(response.getRepeatPeriod().getDayOfWeeks()).hasSize(2)
+                        .containsExactlyInAnyOrder(Day.MON.getId(), Day.FRI.getId()),
+                () -> assertThat(response.getRequiredParticipant()).hasSize(1)
+                        .extracting("recruitmentParticipationNo", "nickName")
+                        .containsExactlyInAnyOrder(tuple(participantNo2, "changHoeun")),
+                () -> assertThat(response.getApprovedParticipant()).hasSize(1)
+                        .extracting("recruitmentParticipationNo", "nickName")
+                        .containsExactlyInAnyOrder(tuple(participantNo1, "soeun"))
+        );
     }
 
 }
