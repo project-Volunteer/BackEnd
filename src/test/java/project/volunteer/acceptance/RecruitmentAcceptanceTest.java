a5fa929a1f1e708723c95ffc3cda20d7c32af70c
BonSik <rnqhstlr2297@gmail.com>
2024-02-14 00:29:04 +0900

    test: 봉사 모집글 삭제 API 인수 테스트 추가

--- src/test/java/project/volunteer/acceptance/RecruitmentAcceptanceTest.java
+++ src/test/java/project/volunteer/acceptance/RecruitmentAcceptanceTest.java
@@ -1,6 +1,7 @@
 package project.volunteer.acceptance;
 
 import static io.restassured.RestAssured.given;
+import static project.volunteer.acceptance.AcceptanceFixtures.봉사_게시물_등록;
 
 import java.io.File;
 import java.util.List;
@@ -54,6 +55,42 @@ public class RecruitmentAcceptanceTest extends AcceptanceTest {
                 .when().post("/recruitment")
                 .then().log().all()
                 .statusCode(HttpStatus.CREATED.value())
+                .extract();
+    }
+
+    @DisplayName("등록한 봉사 모집글을 삭제한다.")
+    @Test
+    void deleteRecruitment() {
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 100, VolunteeringType.IRREG, "01-01-2024", "02-20-2024", HourFormat.AM, "10:00",
+                10,
+                Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        given().log().all()
+                .header(AUTHORIZATION_HEADER, bonsikToken)
+                .when().delete("/recruitment/{recruitmentNo}", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    @DisplayName("방장이 아니면 봉사 모집글을 삭제할 수 없다.")
+    @Test
+    void deleteRecruitmentNotOwner() {
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 100, VolunteeringType.IRREG, "01-01-2024", "02-20-2024", HourFormat.AM, "10:00",
+                10,
+                Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        given().log().all()
+                .header(AUTHORIZATION_HEADER, soeunToken)
+                .when().delete("/recruitment/{recruitmentNo}", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.FORBIDDEN.value())
                 .extract();
     }
 
