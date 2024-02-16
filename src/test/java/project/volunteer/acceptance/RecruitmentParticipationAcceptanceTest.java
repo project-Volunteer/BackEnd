a08c92fac1dda09404e2ad251d894a90516bfef7
BonSik <rnqhstlr2297@gmail.com>
2024-02-16 20:15:08 +0900

    test: 봉사 모집글 팀원 신청 취소 API 인수 테스트 추가

--- src/test/java/project/volunteer/acceptance/RecruitmentParticipationAcceptanceTest.java
+++ src/test/java/project/volunteer/acceptance/RecruitmentParticipationAcceptanceTest.java
@@ -27,7 +27,7 @@ public class RecruitmentParticipationAcceptanceTest extends AcceptanceTest {
     void saveJoin() {
         given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
 
-        Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
                 VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
                 VolunteerType.ADULT, 1000, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
                 10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
@@ -43,17 +43,17 @@ public class RecruitmentParticipationAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("봉사 모집글 인원이 가득찬 경우 팀원 신청을 할 수 없다.")
     @Test
-    void saveJoinFullParticipationNum() {
+    void saveJoinWithFullParticipationNum() {
         given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
 
-        Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
                 VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
                 VolunteerType.ADULT, 2, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
                 10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
                 new File("src/main/resources/static/test/file.PNG"));
 
-        Long recruitmentParticipationNo1 = 봉사_게시물_팀원_가입_요청(soeunToken, recruitmentNo);
-        Long recruitmentParticipationNo2 = 봉사_게시물_팀원_가입_요청(changHoeunToken, recruitmentNo);
+        final Long recruitmentParticipationNo1 = 봉사_게시물_팀원_가입_요청(soeunToken, recruitmentNo);
+        final Long recruitmentParticipationNo2 = 봉사_게시물_팀원_가입_요청(changHoeunToken, recruitmentNo);
 
         final ParticipantAddRequest request = new ParticipantAddRequest(
                 List.of(recruitmentParticipationNo1, recruitmentParticipationNo2));
@@ -72,7 +72,7 @@ public class RecruitmentParticipationAcceptanceTest extends AcceptanceTest {
     void saveJoinDuplicate() {
         given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
 
-        Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
                 VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
                 VolunteerType.ADULT, 10, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
                 10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
@@ -90,10 +90,10 @@ public class RecruitmentParticipationAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("신청 기간이 종료된 봉사 모집글에는 가입 신청을 할 수 없다.")
     @Test
-    void saveJoinDoneRecruitment() {
+    void saveJoinWithDoneRecruitment() {
         given(clock.instant()).willReturn(Instant.parse("2024-02-02T10:00:00Z"));
 
-        Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
                 VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
                 VolunteerType.ADULT, 10, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
                 10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
@@ -102,6 +102,93 @@ public class RecruitmentParticipationAcceptanceTest extends AcceptanceTest {
         given().log().all()
                 .header(AUTHORIZATION_HEADER, soeunToken)
                 .when().put("/recruitment/{recruitmentNo}/join", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract();
+    }
+
+    @DisplayName("봉사 모집글 팀원 신청 취소를 정상적으로 성공한다.")
+    @Test
+    void cancelJoin() {
+        given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
+
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 1000, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
+                10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        봉사_게시물_팀원_가입_요청(soeunToken, recruitmentNo);
+
+        given().log().all()
+                .header(AUTHORIZATION_HEADER, soeunToken)
+                .when().put("/recruitment/{recruitmentNo}/cancel", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    @DisplayName("신청 기간이 종료된 봉사 모집글에는 참여 신청 취소를 할 수 없다.")
+    @Test
+    void cancelJoinWithDoneRecruitment() {
+        given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
+
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 1000, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
+                10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        봉사_게시물_팀원_가입_요청(soeunToken, recruitmentNo);
+
+        given(clock.instant()).willReturn(Instant.parse("2024-02-02T10:00:00Z"));
+
+        given().log().all()
+                .header(AUTHORIZATION_HEADER, soeunToken)
+                .when().put("/recruitment/{recruitmentNo}/cancel", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract();
+    }
+
+    @DisplayName("봉사 모집글 팀원 신청을 하지 않은 회원은 참여 신청 취소를 할 수 없다.")
+    @Test
+    void cancelJoinWithNotJoinRequest() {
+        given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
+
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 1000, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
+                10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        given().log().all()
+                .header(AUTHORIZATION_HEADER, soeunToken)
+                .when().put("/recruitment/{recruitmentNo}/cancel", recruitmentNo)
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract();
+    }
+
+    @DisplayName("봉사 모집글 팀원 승인된 회원은 탐여 신청 취소를 할 수 없다.")
+    @Test
+    void cancelJoinWithInvalidState() {
+        given(clock.instant()).willReturn(Instant.parse("2024-01-29T10:00:00Z"));
+
+        final Long recruitmentNo = 봉사_게시물_등록(bonsikToken,
+                VolunteeringCategory.EDUCATION, "unicef", "11", "1111", "detail", "fullName", 3.2F, 3.2F, true,
+                VolunteerType.ADULT, 1000, VolunteeringType.IRREG, "01-01-2024", "02-01-2024", HourFormat.AM, "10:00",
+                10, Period.NONE, Week.NONE, List.of(), "title", "content", true, false,
+                new File("src/main/resources/static/test/file.PNG"));
+
+        final Long recruitmentParticipationNo1 = 봉사_게시물_팀원_가입_요청(soeunToken, recruitmentNo);
+
+        final ParticipantAddRequest request = new ParticipantAddRequest(List.of(recruitmentParticipationNo1));
+        봉사_게시물_팀원_가입_승인(bonsikToken, recruitmentNo, request);
+
+        given().log().all()
+                .header(AUTHORIZATION_HEADER, soeunToken)
+                .when().put("/recruitment/{recruitmentNo}/cancel", recruitmentNo)
                 .then().log().all()
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .extract();
